36b3e3a5560a901927009f5b02ab2f855c077b62
b2gogaming <47882903+b2gogaming@users.noreply.github.com>
2019-03-22 08:59:41 +0100

    Add files via upload

---
+++ teamspeakvm/addSystemdService.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cat > "/lib/systemd/system/teamspeak.service" <<- EOM
+[Unit]
+Description=TeamSpeak Server
+After=network.target
+[Service]
+WorkingDirectory=/opt/teamspeak-server
+User=teamspeak
+Group=teamspeak
+Type=forking
+ExecStart=/opt/teamspeak-server/ts3server_startscript.sh start inifile=ts3server.ini
+ExecStop=/opt/teamspeak-server/ts3server_startscript.sh stop
+PIDFile=/opt/teamspeak-server/ts3server.pid
+RestartSec=15
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOM
+
+# Reload the systemd manager configuration
+systemctl --system daemon-reload
+
+# Enable and start TeamSpeak server
+systemctl enable --now teamspeak.service
